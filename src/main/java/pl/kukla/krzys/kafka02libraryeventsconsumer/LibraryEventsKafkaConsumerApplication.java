b5ef024cabcc4a1d8d9ebfd6f0a5c7dea27f52f6
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-06-13 17:24:42 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/kafka02libraryeventsconsumer/LibraryEventsKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka02libraryeventsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/kafka02libraryeventsconsumer/LibraryEventsKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka02libraryeventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
